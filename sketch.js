594bfacaf083cf7cc940635cd8d369c10400dbc8
Agustin <Axocuadi@calpoly.edu>
2024-11-11 19:39:27 -0800

    Class Changesr

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let game;
 
+
 function setup() {
   createCanvas(800, 800);
   game = new Game();
@@ -8,4 +9,61 @@ function setup() {
 function draw() {
   background(225);
   game.display();
+}
+
+class Player {
+  constructor(xPos, yPos, size) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.size = size;
+    this.speed = 5; // Speed Movement
+  }
+  draw() {
+    square(this.xPos, this.yPos, this.size);
+  }
+  update() {
+    if (keyIsDown(87)) {
+      // 'W' key (move up)
+      this.yPos -= this.speed;
+    }
+    if (keyIsDown(83)) {
+      // 'S' key (move down)
+      this.yPos += this.speed;
+    }
+    if (keyIsDown(65)) {
+      // 'A' key (move left)
+      this.xPos -= this.speed;
+    }
+    if (keyIsDown(68)) {
+      // 'D' key (move right)
+      this.xPos += this.speed;
+    }
+  }
+}
+class Opponent {
+  constructor(xPos, yPos, size) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.size = size;
+  }
+  draw() {
+    square(this.xPos, this.yPos, this.size);
+  }
+}
+class ShowMap { 
+constructor() { 
+this.player = new Player(200,200,20)
+this.opponents = [new Opponent(100,100,20),
+  new Opponent(50,200,20),
+  new Opponent(50,300,20), 
+  new Opponent(50,350,20)]
+
+}
+display() { 
+  this.player.draw()
+  this.player.update()
+  for(i=0;i<this.opponents.length;i++)
+  this.opponent[i].draw()
+}
+
 }
